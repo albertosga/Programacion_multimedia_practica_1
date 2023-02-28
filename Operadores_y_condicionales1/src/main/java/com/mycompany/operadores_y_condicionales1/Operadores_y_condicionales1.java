03a5d638e867aab3da594b0892ab8071453c7d36
albertosoriano <alberto_soriano@anahuac.mx>
2023-02-27 22:30:16 -0600

    Se corrigio archivo

--- Operadores_y_condicionales1/src/main/java/com/mycompany/operadores_y_condicionales1/Operadores_y_condicionales1.java
+++ Operadores_y_condicionales1/src/main/java/com/mycompany/operadores_y_condicionales1/Operadores_y_condicionales1.java
@@ -16,7 +16,7 @@ public class Operadores_y_condicionales1 {
 //Valor del iva
         double iva = 0.16;
 //Valor de dolar a convertir
-        int USD = 2;
+        double USD = 2;
 //Operacion de dolar a pesos
         double USDMXN = VUSD * USD;
 //Operacion al aplicar iva
@@ -24,7 +24,7 @@ public class Operadores_y_condicionales1 {
 //Imprime cuanto vale el dolar a pesos
         System.out.println("$1 Dolar equivale a: $" + VUSD + " pesos");
 //Indica cuantos dolares hay y los convierte a pesos
-        System.out.println("Se registro $ " + USD + " dolares que equivale a: $ " + USDMXN + " pesos");
+        System.out.println("Se registro $" + USD + " dolares que equivale a:$ " + USDMXN + " pesos");
 //Si el valor de pesos es mayor a 2000 se aplicara el iva del 16%
         if (USDMXN > 2000.00){
 //Imprime el valor total con el iva aplicado
